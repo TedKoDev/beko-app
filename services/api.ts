454f8e60cbf369e07306af5412dfd6b4efdfbc4b
태의 <ted.kordev@gmail.com>
2025-01-31 19:12:47 +0900

    error fix

--- services/api.ts
+++ services/api.ts
@@ -2,9 +2,9 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios, { AxiosError } from 'axios';
 
 // const API_BASE_URL = process.env.EXPO_PUBLIC_BACKEND_URL;
-const API_BASE_URL = 'http://192.168.219.133:3000/api/v1/';
+// const API_BASE_URL = 'http://192.168.219.133:3000/api/v1/';
 // const API_BASE_URL = 'http://localhost:3000/api/v1/';
-// const API_BASE_URL = 'https://api.berakorean.com/api/v1/';
+const API_BASE_URL = 'https://api.berakorean.com/api/v1/';
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
